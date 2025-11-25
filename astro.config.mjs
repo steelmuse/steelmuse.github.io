4a08bd00be96b694a5ca2b5a1313286b90f990ea
Peter Sweeney <peter@steelmuse.com>
2025-11-25 12:39:35 -0500

    Fix animation, improve SEO with sitemap/robots.txt, and add CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://www.steelmuse.com',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
